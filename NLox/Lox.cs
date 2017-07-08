c625ae0828a6a8af553df8563a0146915a61d514
Toby Heighway <toby_hei@outlook.com>
2017-07-08 16:27:20 +1000

    Initial parser

---
+++ NLox/Lox.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace NLox
+{
+    internal class Lox
+    {
+        private static bool hadError;
+
+        private static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: nlox [script]");
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            Run(Convert.ToString(bytes, CultureInfo.InvariantCulture));
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                var line = Console.ReadLine();
+                Console.WriteLine("> ");
+                Run(line);
+            }
+        }
+
+        private static void Run(string source)
+        {
+            var scanner = new Scanner(source);
+            var tokens = scanner.ScanTokens();
+
+            // For now, just print the tokens.
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        public static void error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.WriteLine("[line " + line + "] error" + where + ": " + message);
+            hadError = true;
+        }
+    }
+}
---
+++ NLox/Scanner.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NLox
+{
+    public class Scanner
+    {
+        private static readonly Dictionary<string, TokenType> keywords = new Dictionary<string, TokenType>
+        {
+            {"and", TokenType.AND},
+            {"class", TokenType.CLASS},
+            {"else", TokenType.ELSE},
+            {"false", TokenType.FALSE},
+            {"for", TokenType.FOR},
+            {"fun", TokenType.FUN},
+            {"if", TokenType.IF},
+            {"nil", TokenType.NIL},
+            {"or", TokenType.OR},
+            {"print", TokenType.PRINT},
+            {"return", TokenType.RETURN},
+            {"super", TokenType.SUPER},
+            {"this", TokenType.THIS},
+            {"true", TokenType.TRUE},
+            {"var", TokenType.VAR},
+            {"while", TokenType.WHILE}
+        };
+
+        private readonly string source;
+        private readonly List<Token> tokens = new List<Token>();
+
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!isAtEnd)
+            {
+                // We are at the beginning of the next lexeme.
+                start = current;
+                scanToken();
+            }
+
+            tokens.Add(new Token(TokenType.EOF, "", null, line));
+            return tokens;
+        }
+
+        private bool isAtEnd => current >= source.Length;
+
+        private void scanToken()
+        {
+            switch (advance())
+            {
+                case '(': addToken(TokenType.LEFT_PAREN); break;
+                case ')': addToken(TokenType.RIGHT_PAREN); break;
+                case '{': addToken(TokenType.LEFT_BRACE); break;
+                case '}': addToken(TokenType.RIGHT_BRACE); break;
+                case ',': addToken(TokenType.COMMA); break;
+                case '.': addToken(TokenType.DOT); break;
+                case '-': addToken(TokenType.MINUS); break;
+                case '+': addToken(TokenType.PLUS); break;
+                case ';': addToken(TokenType.SEMICOLON); break;
+                case '*': addToken(TokenType.STAR); break;
+                case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+                case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+                case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+                case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+                case '/' when match('/'):
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd) advance();
+                    break;
+                case '/': addToken(TokenType.SLASH); break;
+                case ' ':
+                case '\r':
+                case '\t':
+                    // Ignore whitespace.
+                    break;
+                case '\n':
+                    line++;
+                    break;
+                case '"': consumeString(); break;
+                case 'o':
+                    if (peek() == 'r')
+                    {
+                        addToken(TokenType.OR);
+                    }
+                    break;
+                case char c when isDigit(c): consumeNumber(); break;
+                case char a when isAlpha(a): consumeIdentifier(); break;
+                default: Lox.error(line, "Unexpected character."); break;
+            }
+        }
+
+        private char advance()
+        {
+            current++;
+            return source[current-1];
+        }
+
+        private void addToken(TokenType type) => addToken(type, null);
+
+        private void addToken(TokenType type, object literal)
+        {
+            var text = source.Substring(start, current - start);
+            tokens.Add(new Token(type, text, literal, line));
+        }
+
+        private bool match(char expected)
+        {
+            if (isAtEnd) return false;
+            if (source[current] != expected) return false;
+
+            current++;
+            return true;
+        }
+
+        private char peek() => isAtEnd ? '\0' : source[current];
+
+        private void consumeString()
+        {
+            while (peek() != '"' && !isAtEnd)
+            {
+                if (peek() == '\n') line++;
+                advance();
+            }
+
+            // Unterminated string.
+            if (isAtEnd)
+            {
+                Lox.error(line, "Unterminated string.");
+                return;
+            }
+
+            // The closing ".
+            advance();
+
+            // Trim the surrounding quotes.
+            var value = source.Substring(start + 1, current - start - 2);
+            addToken(TokenType.STRING, value);
+        }
+
+        private bool isDigit(char c) => c >= '0' && c <= '9';
+
+        private void consumeNumber()
+        {
+            while (isDigit(peek())) advance();
+
+            // Look for a fractional part.
+            if (peek() == '.' && isDigit(peekNext()))
+            {
+                // Consume the "."
+                advance();
+
+                while (isDigit(peek())) advance();
+            }
+
+            addToken(TokenType.NUMBER, double.Parse(source.Substring(start, current-start)));
+        }
+
+        private char peekNext()
+            => current + 1 >= source.Length ? '\0' : source[current + 1];
+
+        private void consumeIdentifier()
+        {
+            while (isAlphaNumeric(peek())) advance();
+
+            // See if the identifier is a reserved word.
+            string text = source.Substring(start, current - start);
+
+            var type = keywords.TryGetValue(text, out var type1) ? type1 : TokenType.IDENTIFIER;
+            addToken(type);
+        }
+
+        private bool isAlpha(char c)
+        {
+            return (c >= 'a' && c <= 'z') ||
+                   (c >= 'A' && c <= 'Z') ||
+                   c == '_';
+        }
+
+        private bool isAlphaNumeric(char c) => isAlpha(c) || isDigit(c);
+    }
+}
